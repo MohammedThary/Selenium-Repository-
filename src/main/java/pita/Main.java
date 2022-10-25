cec77e0299ef4cea8d1767c30c704ef42ae229aa
cipher <mohammed@gmail.com>
2022-10-25 17:53:43 +0300

    hi

--- src/main/java/pita/Main.java
+++ src/main/java/pita/Main.java
@@ -2,6 +2,9 @@ package pita;
 
 public class Main {
     public static void main(String[] args) {
+
+
+        System.out.println("welcome to al shanap world");
         System.out.println("welcome to al shanap world");
     }
 }
